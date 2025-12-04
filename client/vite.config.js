be81f18f971353d4759ea190ee4f4337cd9ef1d4
Rantomampianina <sombiniainarantomampianina@gmail.com>
2025-12-04 09:38:38 +0300

    Modification pour Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']],}, }), tailwindcss(), ],
+  root: '.', // Racine relative au dossier client
+  base: '/', // Pour Vercel
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://rappelbot.onrender.com', // Votre backend sur Render
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
